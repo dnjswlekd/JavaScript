02d249e7c1fc07fbac1d36f0c8fc083ef91f5486
dnjswlekd <wj8014@gmail.com>
2023-10-21 23:55:31 +0900

    Primitive Types 기초적인 유형들

--- js-basic/index.js
+++ js-basic/index.js
@@ -1,10 +1,12 @@
 //This is my first JavaScript code!
 // console.log('hello world')
 
-let name = 'wonji';
-console.log(name);
 
-// Cannot be a reserved keyword 키워드로 지정될 수 없다.
+// variables 변수
+let name1 = 'wonji';
+console.log(name1);
+
+// Cannot be a reserved keyword :키워드로 지정될 수 없다.
 // Should be meaningful
 // Cannot start with a number (1name)
 // Cannot contain a space or hypen (-)
@@ -13,6 +15,16 @@ console.log(name);
 let firstName = 'wonji';
 let lastName = 'Park';
 
+
+
+// constants 상수
 const interestRate = 0.3;
 // interestRate = 1;
 console.log(interestRate)
+
+// Primitive Types 기초적인, 원시적인 유형
+let name2 = 'wonji'; //String literal
+let age= 25; // Number literal
+let inApproved = false; // Boolean literal
+let SecondName = undefined;
+let selectedColor = null
