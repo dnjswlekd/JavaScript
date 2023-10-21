fcba8c52ed5121c5d6dfd1c3d91ab749939668e7
dnjswlekd <wj8014@gmail.com>
2023-10-21 23:43:46 +0900

    variable 변수

--- js-basic/index.js
+++ js-basic/index.js
@@ -1,2 +1,14 @@
 //This is my first JavaScript code!
-console.log('hello world')
+// console.log('hello world')
+
+let name = 'wonji';
+console.log(name);
+
+// Cannot be a reserved keyword 키워드로 지정될 수 없다.
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hypen (-)
+// Are case - sensitive
+
+let firstName = 'wonji';
+let lastName = 'Park';
