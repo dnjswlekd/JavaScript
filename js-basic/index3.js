4d828005299d0ae654a0b1b603c9a5d16704913f
dnjswlekd <wj8014@gmail.com>
2023-10-22 21:40:10 +0900

    Logical Operators with non-boolean 논리연산자

--- js-basic/index3.js
+++ js-basic/index3.js
@@ -12,7 +12,30 @@
 //   console.log('gold');
 // }
 
-let points = 101;
-let type = points > 100 ? 'gold' : 'silver';
+// let points = 101;
+// let type = points > 100 ? 'gold' : 'silver';
 
-console.log(type);
+// console.log(type);
+
+// Logical AND &&
+// Return TRUE if both operands are TRUE
+// 왼쪽 오른쪽 둘다 진실이여야만 진실
+console.log(true && true);
+console.log(false && true);
+console.log(true && false);
+
+// example 대출승인을 위한 신청서 만들기
+
+// Logical OR ||
+// Returns TRUE if one of the operands is TRUE
+// 왼쪽이든 오른쪽이든 둘중에 하나라도 진실이 있다면 진실
+let highIncome = false;
+let goodCreditScore = false;
+
+// let eligibleForLoan = highIncome && goodCreditScore
+let eligibleForLoan = highIncome || goodCreditScore;
+console.log('Eligible',eligibleForLoan);
+
+// Not (!)
+let applicationRefused = !eligibleForLoan;
+console.log('Application Refused', applicationRefused);
