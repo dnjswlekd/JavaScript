e08d7b13f594b08ed1621d9abdc9e051e2b3d88b
dnjswlekd <wj8014@gmail.com>
2023-10-22 17:28:50 +0900

    Assigment Operators 할당 연산자, Comparsion Operators 비교 연산자

--- js-basic/index2.js
+++ js-basic/index2.js
@@ -1,5 +1,5 @@
-let x = 10;
-let y = 4;
+// let x = 10;
+// let y = 4;
 
 // console.log(x + y);
 
@@ -11,10 +11,33 @@ let y = 4;
 // console.log(x % y);
 // console.log(x ** y);
 
-
 //Increment
-console.log(x++);
-console.log(x);
+// console.log(x++);
+// console.log(x);
 
-//Decrement
-console.log(--x);
+// //Decrement
+// console.log(--x);
+
+//Assignment Operators
+// let z = 10;
+
+// // z = z + 5;
+// z += 5;
+
+// // z = z * 5;
+// z *= 5;
+// console.log(z);
+
+//Comparsion Operators
+let x = 1;
+
+
+//관계형 연산자 Relational Operators
+console.log(x > 0);
+console.log(x >= 1);
+console.log(x < 1);
+console.log(x <= 1);
+
+//Equlity
+console.log(x === 1);
+console.log(x !== 1);
