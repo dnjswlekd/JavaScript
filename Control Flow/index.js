4e8a11a64438685e2cca2cf0d5700623ddea3cc3
dnjswlekd <wj8014@gmail.com>
2023-10-24 02:12:45 +0900

    FizzBuzz
    if문 사용 및 나머지 연산
    *NaN typeof -> number

--- Control Flow/index.js
+++ Control Flow/index.js
@@ -1,11 +1,33 @@
-let figure = isLandscape (1920, 720)
-console.log(figure)
+// Divisible by 3 => Fizz
+// Divisible by 5 => Buzz
+// Divisible by both 3 and 5 => FizzBuzz
+// Not Divisible by 3 or 5 => input
+// Not a number => 'Not a number'
 
-console.log(isLandscape(1920, 720))
+// My answer
+// function fizzBuzz(input) {
+//   if(input % 3 === 0 ){
+//     return 'Fizz'
+//   } else if ( input % 5 === 0 ) {
+//     return 'Buzz'
+//   } else if ( input === parseInt(input) ){
+//     return input;
+//   } else return 'Not a number'
+// }
 
-function isLandscape (width, height) {
-  // if (width > height){
-  //   return true;
-  // } return false;
-  return (width > height);
+const output = fizzBuzz(8);
+console.log(output);
+
+// lecture answer
+function fizzBuzz(input) {
+  if (typeof input != 'number') return NaN;
+  
+  if (input % 3 === 0 && input % 5 === 0) return 'FizzBuzz';
+
+  if (input % 3 === 0) return 'Fizz';
+
+  if (input % 5 === 0) return 'Buzz';
+
+
+  return input;
 }
