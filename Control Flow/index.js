5db9ddc751a8094027e5f34ba48b871be2d597e2
dnjswlekd <wj8014@gmail.com>
2023-10-24 00:57:11 +0900

    Break and Continue

--- Control Flow/index.js
+++ Control Flow/index.js
@@ -1,35 +1,10 @@
-// for
-// while
-// do-while
-// 위 루프를 통해 동작을 여러 번 반복할 수 있다.
-
-// 객체의 속성이나 요소의 배열을 반복하는 루프가 더 있다.
-
-// Dot Notation
-person.name
-
-// Bracket Notation
-person['name']
-
-
-
-//for-in
-const person = {
-  name: 'Mosh',
-  age: 30
-};
-
-// for (initialExpresssion; CSSConditionRule; increment)
-for (let key in person)
-console.log(key, person[key])
-// in은 객체의 속성을 반복
-
-const colors = ['red', 'green', 'blue'];
-
-for (let index in colors)
-console.log(index, colors[index]);
-
-// for-of
-for (let color of colors)
-console.log(color)
-// 배열의 요소나 항목을 반복
+let i  = 0;
+while (i <= 10) {
+  if ( i === 5 ) break;
+  if ( i % 2 === 0 ) {
+    i++;
+    continue;
+  }
+  console.log(i);
+  i++;
+}
