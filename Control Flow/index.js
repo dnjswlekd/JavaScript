2627309a2c1497fcd2f604da332b2b2315b7de58
dnjswlekd <wj8014@gmail.com>
2023-10-24 00:22:26 +0900

    Do...While

--- Control Flow/index.js
+++ Control Flow/index.js
@@ -2,12 +2,21 @@
 // for (let i = 1; i <= 5; i++) {
 //   console.log('Hello', i)
 // }
-for (let i = 5; i >= 1; i--) {
-  if (i % 2 !== 0) console.log(i);
-}
+// for (let i = 5; i >= 1; i--) {
+//   if (i % 2 !== 0) console.log(i);
+// }
 
-let i = 0;
-while (i <= 5) {
+// while
+// let i = 0;
+// while (i <= 5) {
+//   if (i % 2 !== 0) console.log(i);
+//   i++;
+// }
+
+// do-while : while안에 있는 조건문이 거짓이더라도 do는 무조건 실행되어짐
+// 쉽게 말해 표현하고 싶은걸 표현 후 조건문을 돌림
+let i = 9;
+do {
   if (i % 2 !== 0) console.log(i);
   i++;
-}
+} while (i <= 5);
