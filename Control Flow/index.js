983eff05b74babddf98afa44b379521f8d5191c0
dnjswlekd <wj8014@gmail.com>
2023-10-24 00:49:53 +0900

    For-of

--- Control Flow/index.js
+++ Control Flow/index.js
@@ -5,6 +5,14 @@
 
 // 객체의 속성이나 요소의 배열을 반복하는 루프가 더 있다.
 
+// Dot Notation
+person.name
+
+// Bracket Notation
+person['name']
+
+
+
 //for-in
 const person = {
   name: 'Mosh',
@@ -14,16 +22,14 @@ const person = {
 // for (initialExpresssion; CSSConditionRule; increment)
 for (let key in person)
 console.log(key, person[key])
-
-// Dot Notation
-person.name
-
-// Bracket Notation
-person['name']
+// in은 객체의 속성을 반복
 
 const colors = ['red', 'green', 'blue'];
 
-for ( let index in colors)
-console.log(index, colors[index])
+for (let index in colors)
+console.log(index, colors[index]);
 
-//for-of
+// for-of
+for (let color of colors)
+console.log(color)
+// 배열의 요소나 항목을 반복
