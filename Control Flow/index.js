ab070ddf0e2ba7ce39ccfa7b6363cf6fac396486
dnjswlekd <wj8014@gmail.com>
2023-10-26 00:29:04 +0900

    Even and Odd Numbers

--- Control Flow/index.js
+++ Control Flow/index.js
@@ -37,3 +37,29 @@ function checkSpeed(speed) {
     console.log('Points:', points);
   }
 }
+
+console.log('---------')
+
+showNumbers(10);
+
+function showNumbers(limit) {
+  for(let i= 0; i <= limit; i++) {
+    const message = 
+    (i % 2 === 0) ? "EVEN" : "ODD"
+    console.log( i, message )
+  }
+}
+
+//My code
+// function showNumbers(limit) {
+//   for(let i= 0; i <= limit; i++) {
+//     (i % 2 === 0) ?
+//       console.log(i, "EVEN") : console.log(i, "ODD")
+//   }
+// }
+
+
+
+
+
+
