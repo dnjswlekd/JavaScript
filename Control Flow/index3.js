54ee0a27ad13a87a53acfeb3d87f2e8f332f4d36
dnjswlekd <wj8014@gmail.com>
2023-10-29 19:34:44 +0900

    Grade

--- Control Flow/index3.js
+++ Control Flow/index3.js
@@ -1,12 +1,34 @@
-console.log(sum(10));
+// console.log(sum(10));
 
-function sum(limit) {
+// function sum(limit) {
+//   let sum = 0;
+
+//   for (let i = 0; i <= limit; i++) {
+//     if (i % 3 === 0 || i % 5 === 0) {
+//       sum += i;
+//     }
+//   }
+//   return sum;
+// }
+
+const marks = [80, 80, 50];
+
+console.log(calculateGrade(marks));
+
+function calculateGrade(marks){
+  const average = calculateAverage(marks)
+  if ( average < 60) return 'F';
+  if ( average < 70) return 'D';
+  if ( average < 80) return 'C';
+  if ( average < 90) return 'B';
+  return 'A';
+}
+
+function calculateAverage(array){
   let sum = 0;
 
-  for (let i = 0; i <= limit; i++) {
-    if (i % 3 === 0 || i % 5 === 0) {
-      sum += i;
-    }
+  for (let value of array){
+    sum += value;
   }
-  return sum;
+  return sum / array.length;
 }
