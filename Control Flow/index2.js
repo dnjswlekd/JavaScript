1ff0744b1fb3ae5f6e76bca271a4caa4d59b7ddb
dnjswlekd <wj8014@gmail.com>
2023-10-29 17:28:14 +0900

    String Properties

--- Control Flow/index2.js
+++ Control Flow/index2.js
@@ -1,14 +1,30 @@
-const array = [ '', undefined, 1, 0, 3, 2];
+// const array = [ '', undefined, 1, 0, 3, 2];
 
-console.log(countTruthy(array))
+// console.log(countTruthy(array))
 
-function countTruthy(array) {
-  let count = 0;
+// function countTruthy(array) {
+//   let count = 0;
 
-  for ( let arr of array) {
-    if ( arr ){
-      count++;
+//   for ( let arr of array) {
+//     if ( arr ){
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+const movie = {
+  title: 'saehun',
+  releaseYear: 1998,
+  rating: 9.1,
+  director: 'o',
+};
+showProperties(movie);
+
+function showProperties(obj) {
+  for ( let key in obj) {
+    if ( typeof obj[key] === 'string'){
+      console.log( key, obj[key])
     }
   }
-  return count;
 }
