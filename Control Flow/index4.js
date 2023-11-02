b310b4f3eda2b0d651131ca2d9c31e25664c69d5
dnjswlekd <wj8014@gmail.com>
2023-11-02 22:03:03 +0900

    Prime Numbers
    소수의 숫자 출력인데 도저히 이해안됨.

---
+++ Control Flow/index4.js
@@ -0,0 +1,25 @@
+// showStarts(5);
+
+// function showStarts(stars) {
+//   for (let star = 5; star < stars; star++) {
+//     let pattern = '';
+//     for (let i = 0; i > star; i++) {
+//       pattern += '*';
+//     }
+//     console.log(pattern);
+//   }
+// }
+
+showPrimes(20);
+
+function showPrimes(limit){
+  for (let number = 2; number <= limit; number++){
+    isPrime = true;
+    for (let factor = 2; factor < number; factor++){
+      if ( number % factor === 0){
+        isPrime = false;
+      }
+    }
+    if (isPrime) console.log(number);
+  }
+}
