4f9c93a77b2f01abfe6ba2868e74d33262fb043f
dnjswlekd <wj8014@gmail.com>
2023-11-02 22:09:11 +0900

    효율적인 코드로 수정

--- Control Flow/index4.js
+++ Control Flow/index4.js
@@ -10,16 +10,19 @@
 //   }
 // }
 
-showPrimes(20);
+showPrimes(2);
 
 function showPrimes(limit){
   for (let number = 2; number <= limit; number++){
-    isPrime = true;
-    for (let factor = 2; factor < number; factor++){
-      if ( number % factor === 0){
-        isPrime = false;
-      }
-    }
     if (isPrime) console.log(number);
   }
+}
+
+function isPrime(number) {
+  for (let factor = 2; factor < number; factor++){
+    if ( number % factor === 0){
+      return false;
+    }
+  }
+  return true;
 }
