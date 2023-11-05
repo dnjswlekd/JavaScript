fc8422c17583a3d04de25e280529c4353425c5fa
dnjswlekd <wj8014@gmail.com>
2023-11-05 15:34:26 +0900

    Date

--- Objects/index.js
+++ Objects/index.js
@@ -1,7 +1,5 @@
-const name = 'wonji'
+const now = new Date();
+const date1 = new Date('Nov 11 2023 15:30');
+const date2 = new Date(2023, 4, 1, 9, 0)
 
-const message = 'This is my\n + first message';
-
-const another =
-`This is ${name}
-first message`
+now.setFullYear(2023)
