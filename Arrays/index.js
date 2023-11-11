ce93e491676c34d83dcdbafa141c0c1562712db4
dnjswlekd <wj8014@gmail.com>
2023-11-12 00:29:35 +0900

    The Spread Operator

--- Arrays/index.js
+++ Arrays/index.js
@@ -1,12 +1,15 @@
-const first = [{ id: 1 }];
+const first = [1, 2, 3];
 const second = [4, 5, 6];
 
 const combine = first.concat(second);
 console.log(combine);
-first[0].id = 10;
 
 const slice = combine.slice(2, 4);
 console.log(slice);
 
-const merge = [...first, ...second];
+const merge = [...first, 'a', ...second, 'b'];
 console.log(merge);
+
+const copy = [...combine];
+
+console.log(copy);
