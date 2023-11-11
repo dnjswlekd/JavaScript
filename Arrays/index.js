d4ee8e5e7135a5390e8233795a2c6df685a91cec
dnjswlekd <wj8014@gmail.com>
2023-11-12 00:27:27 +0900

    Combining and Slicing Arrays

--- Arrays/index.js
+++ Arrays/index.js
@@ -1,8 +1,9 @@
-const first = [1, 2, 3];
+const first = [{ id: 1 }];
 const second = [4, 5, 6];
 
 const combine = first.concat(second);
 console.log(combine);
+first[0].id = 10;
 
 const slice = combine.slice(2, 4);
 console.log(slice);
