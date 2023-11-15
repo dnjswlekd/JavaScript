15109e0e33cfed6ac76fe17cb15f545c4765e6d6
dnjswlekd <wj8014@gmail.com>
2023-11-15 19:43:51 +0900

    Mapping an Array

--- Arrays/index.js
+++ Arrays/index.js
@@ -1,5 +1,9 @@
 const numbers = [1, -1, 2, 3];
 
-const items = numbers.filter((num) => num >= 0).map((num) => ({ value: num }));
+const items = numbers
+  .filter((num) => num >= 0)
+  .map((num) => ({ value: num }))
+  .filter((obj) => obj.value > 1)
+  .map((obj) => obj.value);
 
 console.log(items);
