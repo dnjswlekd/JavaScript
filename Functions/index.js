5171c967088f57545fa202029cb7621790cffd3c
dnjswlekd <wj8014@gmail.com>
2023-12-03 00:12:30 +0900

    The Rest Operator

--- Functions/index.js
+++ Functions/index.js
@@ -1,11 +1,18 @@
-function sum() {
-  let total = 0;
-  console.log(arguments);
+// function sum() {
+//   let total = 0;
+//   console.log(arguments);
 
-  for (num of arguments) {
-    total += num;
-  }
-  return total;
+//   for (num of arguments) {
+//     total += num;
+//   }
+//   return total;
+// }
+
+// console.log(sum(1, 2, 3, 4, 5));
+
+function sum(discount, ...prices) {
+  const total = prices.reduce((arr, cur) => arr + cur);
+  return total * (1 - discount);
 }
 
-console.log(sum(1, 2, 3, 4, 5));
+console.log(sum(0.1, 20, 30));
